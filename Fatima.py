68ad2f4d79eae895f98a26bbb935eb8efd9b4b2f
Eshal-2005 <ramshazafar767@gmail.com>
2025-04-27 14:34:15 +0500

    Add files via upload

--- Fatima.py
+++ Fatima.py
@@ -1,2 +1,7 @@
-from main import OLD
-main()
+import os,time,platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import OLD
+elif bit=='32bit':
+    import OLD32
